68ad558bf7d8266f515c86b72dc75b74bd785220
Matthewit Dechatech <mdechatech@gmail.com>
2015-05-02 14:37:27 -0500

    UI prep for tricks

---
+++ Assets/Scripts/Carousel.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Carousel : MonoBehaviour {
+
+	[SerializeField]
+	private float time;
+
+	[SerializeField]
+	private Transform[] positions;
+
+	private List<Transform> objects;
+
+	private float[] timers;
+
+	void Awake()
+	{
+		objects = new List<Transform>();
+		timers = new float[positions.Length];
+
+		for(int i = 0; i < timers.Length; i++)
+		{
+			timers[i] = 0.0f;
+		}
+	}
+
+	public void addObject(Transform transform)
+	{
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
