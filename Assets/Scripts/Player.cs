a7acc29c1a68801544ae2fd1a28fc30626456031
vladislav manoylo <vladislav.manoylo@gmail.com>
2015-05-02 17:36:01 -0500

    speed 8

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -67,7 +67,7 @@ public class Player : MonoBehaviour {
 	void Start () {
 		mainUI.scoreText.text = "0";
 
-		speed = 45;
+		speed = 8;
 		leftSkiDirection = 0;
 		rightSkiDirection = 0;
 		height = 0;
